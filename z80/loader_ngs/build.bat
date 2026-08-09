82d3b7f637deff3638382fe3313b6a1796c266ea
savelij <savelij@84fbd9f8-8684-4fd2-aa7d-35f94c61736f>
2026-08-09 13:13:57 +0000

    test_ngs: - добавлено тестирование переключений частот процессора.
    git-svn-id: svn://svn.nedopc.com/ngs@191 84fbd9f8-8684-4fd2-aa7d-35f94c61736f

--- z80/loader_ngs/build.bat
+++ z80/loader_ngs/build.bat
@@ -1,5 +1,23 @@
 
-@ECHO OFF
+@echo off
 
-..\..\tools\asw\asw -cpu z80undoc -U -L loader_ngs.a80
-..\..\tools\asw\p2bin loader_ngs.p loader_ngs.rom -r $-$ -k
+if not exist tmp md tmp
+call setpath_tools
+call setpath_includes
+
+cls
+
+asl -U -L -C -x -olist tmp\loader.lst -i %includes% loader_ngs.a80 || goto error
+p2bin loader_ngs.p loader_ngs.rom -r $-$ -k
+
+echo ########################
+echo # -= End Compile Ok =- #
+echo ########################
+goto exit
+
+:error
+echo #######################
+echo # -= Error Compile =- #
+echo #######################
+
+:exit
